fb13e0cc2f17b86c0c08757f9185923f5c2420df
pratapmajge <majgepratap1504@gmail.com>
2025-03-20 13:18:40 +0530

    dist

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // Ensure the build output is stored in the "dist" folder
+    outDir: resolve(__dirname, 'dist') // Ensure the correct output path
   }
 })
